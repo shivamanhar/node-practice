047502fc06f2fa7642423d8d13c09c994c139172
shivamanhar@gmail.com <shivamanhar@gmail.com>
2021-07-30 11:26:05 +0530

    HTML file read through fs package

--- project1/new-app.js
+++ project1/new-app.js
@@ -1,8 +1,27 @@
 const http = require('http');
+const {readFileSync} = require('fs');
 
+const homePage = readFileSync('./home.html');
+const aboutPage = readFileSync('./about.html');
 const server = http.createServer((req, res)=>{
-    res.write("<h1> Hello </h1>");
+    //req.method
+    //req.url
+    if(req.url === '/')
+    {
+        res.writeHead(200, {'content-type':'text/html'});
+        res.write(homePage);
+        res.end();
+    }
+    if(req.url === '/about')
+    {
+        res.writeHead(200, {'content-type':'text/html'});
+        res.write(aboutPage);
+        res.end();
+    }
+    res.writeHead(404, {'content-type':'text/html'});
+    res.write("Page not found");
     res.end();
+    
 
 });
 
