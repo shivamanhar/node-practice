f032cbb5c30734769921e9b87b3957114ad3ee9b
Shiva Manhar <shivamanhar@gmail.com>
2021-08-26 04:08:21 +0530

    Graph Traversal

---
+++ breathFirst-traversal.js
@@ -0,0 +1,25 @@
+const breathFirstPrint = (graph, source) =>{
+    const queue = [source];
+    while (queue.length > 0)
+    {
+        const current = queue.shift();
+        console.log(current);
+        for (let neighbor of graph[current])
+        {
+            queue.push(neighbor);
+        }
+    }
+}
+
+const graph = {
+    a:['b', 'c'],
+    b:['d'],
+    c:['e'],
+    d:['f'],
+    e:[],
+    f:[]
+}
+
+breathFirstPrint(graph, 'a');
+
+// acyclic = no cycles
