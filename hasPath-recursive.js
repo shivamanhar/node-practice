f7954b0add3475b37c9585cb1a0be4c244a48d04
Shiva Manhar <shivamanhar@gmail.com>
2021-08-27 05:38:43 +0530

    Undirected-graph-problem

---
+++ hasPath-recursive.js
@@ -0,0 +1,25 @@
+// has path breathfirst solution
+
+const hasPath = (graph, src, dst) =>{
+    const queue = [src];
+    while (queue.length > 0){
+        const current = queue.shift();
+        if (current === dst) return true;
+        for (let neighbor of graph[current]){
+            queue.push(neighbor);
+        }
+    }
+    return false;
+};
+
+const  graph = {
+    f:['g', 'i'],
+    g:['h'],
+    h:[],
+    i:['g', 'k'],
+    j:['i'],
+    k:[]
+};
+
+result = hasPath(graph, 'f', 'k');
+console.log(result);
